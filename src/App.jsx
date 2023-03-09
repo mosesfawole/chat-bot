0fd3f85032db17a78e03366699d9c60be84c4c4c
Moses Fawole <mosesfawole2001@gmail.com>
2023-03-09 14:08:08 +0100

    chat functionalty

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,9 @@ function App() {
       sender: "ChatGeePeeTEe",
     },
   ]);
+  const [typing, setTyping] = useState(false);
+
+  const apiKey = import.meta.env.VITE_OPENAI_KEY;
 
   const handleSubmit = async (message) => {
     const newMessage = {
@@ -28,14 +31,72 @@ function App() {
     // update messages state
     setMessages(newMessages);
 
+    // set typing indicator
+    setTyping(true);
+    await processMessages(newMessages);
     // process mesaage to chatgpt
+  };
+
+  const processMessages = async (chatMessages) => {
+    let apiMessages = chatMessages.map((messageObj) => {
+      let role = "";
+      if (messageObj.sender === "ChatGPT") {
+        role = "assistant";
+      } else {
+        role = "user";
+      }
+      return { role: role, content: messageObj.message };
+    });
+
+    // role: "user" =. a message from the user, 'assistant' =. a response from chatGpt
+    // system == generally one initial message defining How we want chat gpt to talk
+    const systemMessage = {
+      role: "system",
+      content: "Speak like a pirate",
+    };
+    const apiRequestBody = {
+      model: "gpt-3.5-turbo",
+      messages: [systemMessage, ...apiMessages],
+    };
+
+    await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(apiRequestBody),
+    })
+      .then((data) => {
+        return data.json();
+      })
+      .then((data) => {
+        // console.log(data);
+        // console.log(data.choices[0].message.content);
+        setMessages([
+          ...chatMessages,
+          {
+            message: data.choices[0].message.content,
+            sender: "ChatGPT",
+          },
+        ]);
+        setTyping(false);
+      });
   };
   return (
     <div className="App">
       <div className="box">
         <MainContainer>
           <ChatContainer>
-            <MessageList>
+            <MessageList
+              color="green"
+              scrollBehavior="smooth"
+              typingIndicator={
+                typing ? (
+                  <TypingIndicator content="ChatGeePeeTee is typing" />
+                ) : null
+              }
+            >
               {messages.map((message, index) => (
                 <Message key={index} model={message} />
               ))}
